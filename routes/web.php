c618eba230696098de6546c8acd46239d209c900
Jasom Rocha <jasomrocha27@gmail.com>
2026-05-26 00:03:59 -0300

    AULA 3: Uma rota, vários verbos

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,14 @@ use Illuminate\Support\Facades\Route;
 // Rotas nomeadas tornam a URI flexível, pois o que define a rota é o nome dela definido no "name"
 Route::get('/', [SiteController::class, 'home'])->name('site.home');
 Route::get('/contato', [SiteController::class, 'contact'])->name('site.contact');
+
+// AULA 3 - UMA ROTA, VÁRIOS VERBOS
+Route::match(['PUT', 'PATCH'], '/usuario/{id}', function(){
+    // Aceita a combinação passada no primeiro parâmetro
+});
+
+Route::any('/usuario/{id}', function(){
+  // Aceita qualquer verbo, a própria documentação oficial desencoraja o uso
+});
+
+// php artisan route:list --except-vendor -> Lista todas as rotas configuradas
